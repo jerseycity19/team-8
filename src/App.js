f1646da8ad9d7399e6e00306eabaa63781eef528
meriselleb <meri.selle@yahoo.com>
2019-09-27 19:51:10 -0400

    added firebase module

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/globalnomadsgroup/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Join our facebook page!
         </a>
       </header>
     </div>

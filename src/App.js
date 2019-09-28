48e58542a9ebbd57dc4dddeed358aea9bc0180b7
meriselleb <meri.selle@yahoo.com>
2019-09-27 21:21:42 -0400

    firebase and api keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: "",
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  };
+ firebase.initializeApp(firebaseConfig);
+
 var provider = new firebase.auth.FacebookAuthProvider();
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  handleOnClick() {
+    firebase.auth().signInWithRedirect(provider);
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleOnClick}>
+          Log in
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import * as firebase from "firebase/app";
+// import * as firebase from "firebase/app";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyC4_Cf-2gjJf-s3S_2bVlbBaz20b2p4VE0",
-    authDomain: "jpmorgancodeforgoodteam8.firebaseapp.com",
-    databaseURL: "https://jpmorgancodeforgoodteam8.firebaseio.com",
-    projectId: "jpmorgancodeforgoodteam8",
-    storageBucket: "",
-    messagingSenderId: "239208028690",
-    appId: "1:239208028690:web:576b8307e3e9c2396c16cb",
-    measurementId: "G-HY2XEWS90N"
-}
-firebase.initializeApp(firebaseConfig);
 
+// var firebaseConfig = {
+//     apiKey: "AIzaSyC4_Cf-2gjJf-s3S_2bVlbBaz20b2p4VE0",
+//     authDomain: "jpmorgancodeforgoodteam8.firebaseapp.com",
+//     databaseURL: "https://jpmorgancodeforgoodteam8.firebaseio.com",
+//     projectId: "jpmorgancodeforgoodteam8",
+//     storageBucket: "",
+//     messagingSenderId: "239208028690",
+//     appId: "1:239208028690:web:576b8307e3e9c2396c16cb",
+//     measurementId: "G-HY2XEWS90N"
+//   };
+//  firebase.initializeApp(firebaseConfig);

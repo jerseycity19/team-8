90470244e6e037238723bccd61c7c180e51625ec
meriselleb <meri.selle@yahoo.com>
2019-09-28 02:05:06 -0400

    buttons more

--- src/EventItem.js
+++ src/EventItem.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './App.css';
+
+const buttonTwo = {
+  backgroundColor: '#627BAF',
+  background: 'groove',
+  fontColor: 'black',
+};
+
 
 export const EventItem = props => {
   return(
-    <div>
+      <div>
+      <button style={buttonTwo}>
       <p>Topic: {props.topic}</p>
       <p>StarTime: {props.startTime}</p>
       <p>endTime: {props.endTime}</p>
+      </button>
     </div>
   )
 };

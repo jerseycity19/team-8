8c551e4d756a1e668501303d42246c18e1e3e500
edwincarbajal <exaviercarbajal@gmail.com>
2019-09-28 07:04:26 -0400

    Fix webcam rendering and auth state

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import "firebase/auth";
 import './App.css';
 import './index.css';
 import EventList from './EventList';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
 import Verify from './Verify';
-
 
 class App extends React.Component {
   constructor(props) {
@@ -22,14 +21,14 @@ class App extends React.Component {
     firebase.auth().getRedirectResult().then(function(result) {
     if (result.credential) {
       let token = result.credential.accessToken;
-      if(token) {
-        _this.setState({
-          user,
-          isLoggedIn: true
-        });
-      }
     }
     let user = result.user;
+    if(user) {
+      _this.setState({
+        user,
+        isLoggedIn: true
+      });
+    }
     }).catch(function(error) {
       let errorCode = error.code;
       let errorMessage = error.message;
@@ -57,66 +56,69 @@ class App extends React.Component {
     // TO BE CODED ONCE EVENTS ARE MADE
   }
 
-
-
   render() {
     const { isLoggedIn } = this.state;
     return (
       <div className="App">
-        {isLoggedIn ?
-          (
-          <>
-          <EventList firebase={firebase} />
-          <img src="/images/1.jpg" alt=""/>
+        <img src="/images/1.jpg" alt=""></img>
         <h1>GLOBAL NOMADS GROUP</h1>
         <h2 className="missionStatement">
           Global Nomads Group connects youth from around
           the world to engage across lines of difference.
         </h2>
-        <button className="ui button" onClick={this.handleOnClick}>
-          Log in
-        </button>
-        <ul> Future RSVP'd Tables: <p></p><li>Table Talk September 30th 11 am <p>Climate Change</p></li><li>Table Talk October 22th 3pm <p>Gender Norms</p></li>
-        </ul>
-        <h3>
-          Upcoming and Current Tables:
-        </h3>
-        <button className="ui purple button" onClick={this.eventOnClick}>Table Talk<p></p>September 28th 2pm <p></p>  <p>Free Table</p>
-        </button>
-        <button className="ui purple button" onClick={this.eventOnClick}>
-          Table Talk <p></p> September 30th 11am <p></p> <p></p> Climate Change
-        </button>
-        <h4>
-          Topic-Specific Tables:
-        </h4>
-        <button className="ui purple button" onClick={this.eventOnClick}>
-          Table Talk <p></p> October 11th 11am
-          <p></p> <p></p> Giving to the Community
-        </button>
-        <h5>
-          Free Tables (No Set Topic):
-        </h5>
-        {isLoggedIn ?
-          (<EventList firebase={firebase} />) // HERE.
-        : (
-          <button onClick={this.handleOnClick}>
-            Log in
-          </button>
-        )
-        }
-        <h6>
-          Create Your Own Table!
-        </h6>
-          <>
-            <Verify />
-            <button onClick={this.handleOnClick}>
-              Log in
-            </button>
-          </>
-        )
-        }
-      </div>
 
+        {isLoggedIn ?
+          (
+            <>
+              <EventList firebase={firebase} />
+              <ul> Future RSVP'd Tables:
+                <li>Table Talk September 30th 11 am
+                  <p>
+                    Climate Change
+                  </p>
+                </li>
+                <li>Table Talk October 22th 3pm
+                  <p>Gender Norms</p>
+                </li>
+              </ul>
+
+              <h3>
+                Upcoming and Current Tables:
+              </h3>
+
+              <button
+                className="ui purple button"
+                onClick={this.eventOnClick}
+              >
+                <p>Table Talk</p><p>September 28th 2pm</p> <p>Free Table</p>
+              </button>
+
+              <button className="ui purple button" onClick={this.eventOnClick}>
+                Table Talk <p></p> September 30th 11am <p></p> <p></p> Climate Change
+              </button>
+
+              <h4>
+                Topic-Specific Tables:
+              </h4>
+
+              <button className="ui purple button" onClick={this.eventOnClick}>
+                Table Talk <p></p> October 11th 11am
+                <p></p> <p></p> Giving to the Community
+              </button>
+
+              <h5>
+                Free Tables (No Set Topic):
+              </h5>
+            </>
+          ) : (
+                <>
+                  <Verify />
+                  <button onClick={this.handleOnClick}>
+                    Log in
+                  </button>
+                </>
+          )}
+      </div>
     );
   }
 }

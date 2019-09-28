818970fbbfe0377f147bae5f95b1df4ac16b8634
Damian Jachyra <damianjachyra@Damians-MacBook-Pro.local>
2019-09-27 23:22:39 -0400

    added a new file

--- login.py
+++ login.py
@@ -1,1 +1,21 @@
 # script to log into a website
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Safari/605.1.15'
+
+POST_LOGIN_URL = "https://zoom.us/signin"
+
+REQUEST_URL = "https://zoom.us/profile"
+
+payload = {
+    'email': 'damianjachyra@gmail.com',
+    'password': 'Test123!'
+}
+
+with requests.Session() as session:
+    post = session.post(POST_LOGIN_URL, data=payload)
+    r = session.get(REQUEST_URL)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    print(soup)

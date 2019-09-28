9e98b6521041144d3faf8b15756f8fdbbeb91204
meriselleb <meri.selle@yahoo.com>
2019-09-27 20:13:32 -0400

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+var provider = new firebase.auth.FacebookAuthProvider();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/globalnomadsgroup/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Join our facebook page!
-        </a>
-      </header>
+     hello world
+
     </div>
   );
 }
